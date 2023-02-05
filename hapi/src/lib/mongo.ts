b156599e8b4edc6fd7b416ba0951e413f08a121f
raquelmgalbuquerque <81765141+raquelmgalbuquerque@users.noreply.github.com>
2023-02-05 17:37:10 +0000

    first commit

---
+++ hapi/src/lib/mongo.ts
@@ -0,0 +1,37 @@
+import Hapi from "@hapi/hapi";
+import hapiMongo from "hapi-mongodb";
+
+/**
+ * `mongo@1.0.0`
+ * - Register the 'hapi-mongodb'
+ */
+export default Object.freeze<Hapi.Plugin<void>>({
+  name: "mongo",
+  version: "1.0.0",
+  register: async (server) => {
+    const usr = "db_user";
+    const pwd = "12345";
+    const options: hapiMongo.Options = {
+      url: `mongodb+srv://${usr}:${pwd}@edit-backend-jan-2023.iahzmxi.mongodb.net/sample_mflix?retryWrites=true&w=majority`,
+      settings: { useUnifiedTopology: true },
+      decorate: true,
+    };
+
+    await server.register({
+      plugin: hapiMongo,
+      options,
+    });
+  },
+});
+
+declare module "@hapi/hapi" {
+  // extends typeof `Hapi.Server`
+  interface Server {
+    mongo: hapiMongo.HapiMongo;
+  }
+
+  // extends typeof `Hapi.Request`
+  interface Request {
+    mongo: hapiMongo.HapiMongo;
+  }
+}
