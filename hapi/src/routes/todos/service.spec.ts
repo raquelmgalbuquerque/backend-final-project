96497961ef9fb813ed26641013099881743fc554
raquelmgalbuquerque <81765141+raquelmgalbuquerque@users.noreply.github.com>
2023-02-09 23:32:09 +0000

    add commented solution for service tests

--- hapi/src/routes/todos/service.spec.ts
+++ hapi/src/routes/todos/service.spec.ts
@@ -1,3 +1,5 @@
+import { makeChance } from "../../lib/chance";
+
 import {
   getAll as _getAll,
   getOne as _getOne,
@@ -5,48 +7,51 @@ import {
   update as _update,
   remove as _remove,
   search as _search,
-} from './service'
+} from "./service";
 
+const chance = makeChance();
 
-describe('getAll', () => {
-  it('implement tests', async () => {
-    // const res = await getAll()
-    // expect(res).toEqual('from getAll')
-  })
-})
+describe("getAll", () => {
+  it("implement tests", async () => {
+    // const offset = chance.integer({ min: 0, max: 9 });
+    // const limit = chance.integer({ min: 0, max: 9 });
+    // const order = chance.natural({ min: -1, max: 1, exclude: [0] });
+    // const res = await _getAll(mongo, offset, limit, 1);
+    // expect(res).toEqual("from getAll");
+  });
+});
 
-describe('getOne', () => {
-  it('implement tests', async () => {
+describe("getOne", () => {
+  it("implement tests", async () => {
     // const res = await getOne()
     // expect(res).toEqual('from getOne')
-  })
-})
+  });
+});
 
-describe('create', () => {
-  it('implement tests', async () => {
+describe("create", () => {
+  it("implement tests", async () => {
     // const res = await create()
     // expect(res).toEqual('from create')
-  })
-})
+  });
+});
 
-describe('update', () => {
-  it('implement tests', async () => {
+describe("update", () => {
+  it("implement tests", async () => {
     // const res = await update()
     // expect(res).toEqual('from update')
-  })
-})
+  });
+});
 
-describe('remove', () => {
-  it('implement tests', async () => {
+describe("remove", () => {
+  it("implement tests", async () => {
     // const res = await remove()
     // expect(res).toEqual('from remove')
-  })
-})
+  });
+});
 
-describe('search', () => {
-  it('implement tests', async () => {
+describe("search", () => {
+  it("implement tests", async () => {
     // const res = await search()
     // expect(res).toEqual('from search')
-  })
-})
-
+  });
+});
